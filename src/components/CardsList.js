cd2b95f1f282849583af3c56ca65c727f054f6c6
Charlie Wallace <charlescwallace2017@gmail.com>
2022-10-06 21:15:52 -0400

    Migrate flip card classes & init header

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,23 +1,25 @@
-import { Col, Row } from 'reactstrap';
-import PictureCard from './PictureCard';
-import { PARKS } from '../shared/PARKS';
+import { Container, Col, Row } from "reactstrap";
+import PictureCard from "./PictureCard";
+import { PARKS } from "../shared/PARKS";
 
 const CardsList = () => {
     const parks = PARKS;
 
     return (
-        <Row>
-            {parks.map((park) => {
-                return (
-                    park && (
-                        <Col key={park.id}>
-                            <PictureCard park={park} />
-                        </Col>
-                    )
-                )
-            })}
-        </Row>
-    )
-}
+        <Container className="d-flex">
+            <Row>
+                {parks.map((park) => {
+                    return (
+                        park && (
+                            <Col sm='4' key={park.id}>
+                                <PictureCard park={park} />
+                            </Col>
+                        )
+                    );
+                })}
+            </Row>
+        </Container>
+    );
+};
 
 export default CardsList;
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,17 +1,24 @@
-import { Card, CardBody, CardImg, CardTitle } from 'reactstrap';
-import { PARKS } from '../shared/PARKS';
+import { Card, CardBody, CardImg } from "reactstrap";
+import { PARKS } from "../shared/PARKS";
 
 const PictureCard = ({ park }) => {
     const { image, name } = park;
 
     return (
-        <Card>
-            <CardImg src={image} alt={name} />
-            <CardBody>
-                <CardTitle>{name}</CardTitle>
-            </CardBody>
-        </Card>
-    )
-}
+        <div className="flip-card mb-3">
+            <div className="flip-card-inner">
+                <Card>
+                    <CardImg top src={image} alt={name} className="h-75 mb-3" />
+                    <CardBody className="d-flex align-items-center justify-content-center">
+                        <h4 className="text-center">{name}</h4>
+                    </CardBody>
+                </Card>
+                <div className="flip-card-back">
+                    <p>This is a note about the trail.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default PictureCard;
